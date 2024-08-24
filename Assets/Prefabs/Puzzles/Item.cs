b44add2b7671c8a2d5badbe3ae15c84530b8c2b7
Megan Lucas <1017630@student.sae.edu.com>
2024-08-24 18:09:03 +0800

    started item id and puzzle scripts

---
+++ Assets/Prefabs/Puzzles/Item.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public bool inWorld = true;
+    public bool inInventory = false;
+    public int itemID = 0;
+    
+
+   //public void Pickup()
+   //{ 
+   ////make all Item's renderer invisable
+   ////move item to inventory (space?)
+   //}
+}
--- Assets/Prefabs/Puzzles/Key.cs
+++ Assets/Prefabs/Puzzles/Key.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+
+    public Item thisKey = new Item();
+    public enum PuzzleID {Yellow = 1,Orange = 2,Red = 3,Blue = 4,Purple = 5, Green = 6}
+    public PuzzleID puzzleID;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        // itemID is set to the puzzleID number
+        thisKey.itemID = (int)puzzleID;
     }
 
     // Update is called once per frame
